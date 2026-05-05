e030c756bbf874fcd4f9bb52a97ef5b5aa5b5ea4
kawya09 <h7346917@gmail.com>
2026-05-05 12:02:01 +0530

    jhgf

--- CSS/script.js
+++ CSS/script.js
@@ -1,3 +1,27 @@
-console.log("Wow");
-console.log("Hi");
-console.log("Welcome");
+
+    // Navbar toggle
+    const menuIcon = document.querySelector('#menu-icon');
+    const navbar = document.querySelector('.navbar');
+    menuIcon.onclick = () => {
+    navbar.classList.toggle('active');
+}
+
+    // Smooth Scroll Active Link
+    let sections = document.querySelectorAll('section');
+    let navLinks = document.querySelectorAll('header nav a');
+
+    window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+};
